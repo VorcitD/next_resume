83485d13cfdc9391d9b2aa3979ebc806810e81e6
Victor Cavalcanti <vitimendoza@gmail.com>
2024-01-25 17:59:33 -0300

    [ NR-8 ] Add Logo

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -9,16 +9,15 @@ export default function Footer(){
       <section className="h-60 pt-10 flex flex-col items-center justify-center">
         <div className="flex justify-between items-center w-11/12">
         <div className="flex flex-col w-full h-full justify-start items-start">
-          <Image className="mb-5" src="/next.svg" width="165" height="79" alt={"Logo"}></Image>
-          <p className="text-md text-fontSecondary ">Lorem ipsum dolor sit, </p>
+        <Image src="/LogoVCBranca.png" width="180" height="79" alt={"Logo"}></Image>
           </div>
         <div className="h-full flex flex-col justify-start items-center w-full ">
             <nav className="text-sm text-fontSecondary font-thin tracking-widest fill-fontPrimary font-serif w-full flex justify-end items-center ">
-                <Link className="px-6 pb-2 hover:text-secondary transition duration-300 " href="">Home</Link>
-                <Link className="px-6 pb-2 hover:text-secondary transition duration-300 " href="">About</Link>
-                <Link className="px-6 pb-2 hover:text-secondary transition duration-300 " href="">Portfolio</Link>
+                <Link className="px-6 pb-2 hover:text-secondary transition duration-300 " href="">Inicio</Link>
+                <Link className="px-6 pb-2 hover:text-secondary transition duration-300 " href="">Sobre</Link>
+                <Link className="px-6 pb-2 hover:text-secondary transition duration-300 " href="">Portifolio</Link>
                 <Link className="px-6 pb-2 hover:text-secondary transition duration-300 " href="">Blog</Link>
-                <Link className="px-6 pb-2 hover:text-secondary transition duration-300 " href="">Contacts</Link>
+                <Link className="px-6 pb-2 hover:text-secondary transition duration-300 " href="">Contatos</Link>
             </nav>
             <div className="w-full flex items-center justify-end pt-5 pb-12 ">
             <SocialMediaMenu></SocialMediaMenu>
--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -7,15 +7,15 @@ export default function Header() {
     return (
       <section className="h-32 pt-10 w-screen flex justify-between items-center px-8">
         <div className="pl-9">
-          <Image src="/next.svg" width="165" height="79" alt={"Logo"}></Image>
+          <Image src="/LogoVCBranca.png" width="180" height="79" alt={"Logo"}></Image>
           </div>
         <div className="h-full p-5 flex justify-center items-center">
             <nav className="text-lg text-fontPrimary font-thin tracking-widest fill-fontPrimary font-serif h-full flex justify-center items-center ">
-                <Link className="px-7 pb-2 hover:text-secondary hover:border-b hover:border-secondary transition duration-300 " href="">Home</Link>
-                <Link className="px-7 pb-2 hover:text-secondary hover:border-b hover:border-secondary transition duration-300 " href="">About</Link>
-                <Link className="px-7 pb-2 hover:text-secondary hover:border-b hover:border-secondary transition duration-300 " href="">Portfolio</Link>
+                <Link className="px-7 pb-2 hover:text-secondary hover:border-b hover:border-secondary transition duration-300 " href="">Inicio</Link>
+                <Link className="px-7 pb-2 hover:text-secondary hover:border-b hover:border-secondary transition duration-300 " href="">Sobre</Link>
+                <Link className="px-7 pb-2 hover:text-secondary hover:border-b hover:border-secondary transition duration-300 " href="">Portifolio</Link>
                 <Link className="px-7 pb-2 hover:text-secondary hover:border-b hover:border-secondary transition duration-300 " href="">Blog</Link>
-                <Link className="px-7 pb-2 hover:text-secondary hover:border-b hover:border-secondary transition duration-300 " href="">Contacts</Link>
+                <Link className="px-7 pb-2 hover:text-secondary hover:border-b hover:border-secondary transition duration-300 " href="">Contatos</Link>
             </nav>
         </div>
         <div className="pr-9">
