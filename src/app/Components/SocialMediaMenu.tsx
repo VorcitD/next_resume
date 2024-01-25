1b7a14b9a88d6c24786b47214ecd0766020f668d
Victor Cavalcanti <vitimendoza@gmail.com>
2024-01-25 12:43:47 -0300

    [NR-06] Add Contacts to Social Media Menu

--- src/app/Components/SocialMediaMenu.tsx
+++ src/app/Components/SocialMediaMenu.tsx
@@ -7,10 +7,10 @@ import Link from "next/link";
 export default function SocialMediaMenu(){
     return(
         <div className="flex">
-            <Link className="text-secondary text-xl mr-8 hover:cursor-pointer hover:text-fontSecondary" href={""}><FaWhatsapp/></Link>
-            <Link className="text-secondary text-xl mr-8 hover:cursor-pointer hover:text-fontSecondary" href={""}><FiGithub/></Link>
-            <Link className="text-secondary text-xl mr-8 hover:cursor-pointer hover:text-fontSecondary" href={""}><FiLinkedin/></Link>
-            <Link className="text-secondary text-xl mr-8 hover:cursor-pointer hover:text-fontSecondary" href={""}><FiMail/></Link>
+            <Link className="text-secondary text-xl mr-8 hover:cursor-pointer hover:text-fontSecondary" href={"https://wa.me//5562999189082?text=Olá Victor! Vi seu currículo e me interessei pelos seus serviços!"}><FaWhatsapp/></Link>
+            <Link className="text-secondary text-xl mr-8 hover:cursor-pointer hover:text-fontSecondary" href={"https://github.com/VorcitD"}><FiGithub/></Link>
+            <Link className="text-secondary text-xl mr-8 hover:cursor-pointer hover:text-fontSecondary" href={"https://www.linkedin.com/in/victormendozac/"}><FiLinkedin/></Link>
+            <Link className="text-secondary text-xl mr-8 hover:cursor-pointer hover:text-fontSecondary" href={"mailto:vitimendoza@gmail.com"}><FiMail/></Link>
         </div>
     );
 }
